e25d7369cc27dc98b0871207c66b4bf64c865b75
cassiotakarada <102415472+cassiotakarada@users.noreply.github.com>
2022-12-29 13:14:08 -0300

    Updating Studies

--- week3/TypeScript/dist/app.js
+++ week3/TypeScript/dist/app.js
@@ -1,6 +1,23 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.describe();
 //# sourceMappingURL=app.js.map
